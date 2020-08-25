b50ede2dd8d904a7b69b26ba0643ccb34c0a4997
Tony380 <tonygon2@yahoo.es>
2020-08-25 13:53:13 -0400

    Heroku deployment

--- purbeurre/views.py
+++ purbeurre/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 
 
 def index(request):
+    """ Main page """
     return render(request, 'index.html')
 
 
 def legal(request):
+    """ Legal mentions page """
     return render(request, 'legal.html')
 
 
 def my_404_view(request, exception):
+    """ Error 404 page """
     return render(request, '404.html')
 
 
 def my_500_view(request):
+    """ Error 500 page """
     return render(request, '500.html')
