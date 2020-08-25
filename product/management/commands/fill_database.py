3cde2dba64e694b6ac4c205badfca660a513f1e7
Tony380 <tonygon2@yahoo.es>
2020-08-25 14:36:31 -0400

    Heroku deployment

--- product/management/commands/fill_database.py
+++ product/management/commands/fill_database.py
@@ -10,31 +10,39 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Chosen categories
-        name = ["Pâtes à tartiner aux noisettes et au cacao", "Muffins", "Biscuits", "Tortellini", "Viennoiseries",
+        name = ["Pâtes à tartiner aux noisettes et au cacao",
+                "Muffins", "Biscuits", "Tortellini", "Viennoiseries",
                 "Taboulés", "Confitures", "Cassoulets", "Yaourts", "Sodas"]
         for element in name:
             self.stdout.write('Import de la catégorie {}'.format(element))
             cat = Category(name=element)
             cat.save()
-            self.stdout.write('Import des produits de la catégorie {}'.format(element))
-            payload = {"search_terms": "{}".format(cat), "page_size": 50, "json": 1}
-            res = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?", params=payload)
+            self.stdout.write(
+                'Import des produits de la catégorie {}'.format(element))
+            payload = {"search_terms": "{}".format(cat),
+                       "page_size": 50, "json": 1}
+            res = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?",
+                               params=payload)
             result = res.json()
             products = result["products"]
             for i in products:
                 # avoid products with missing data
                 try:
-                    if i.get("product_name", False) and i.get("brands", False) and \
-                            i.get("nutrition_grades", False) and i.get("image_front_url", False):
-                        product = Product.objects.create(name=i["product_name"],
-                                                         brands=i["brands"],
-                                                         link=i["url"],
-                                                         nutriscore=i["nutrition_grades"],
-                                                         image=i["image_front_url"],
-                                                         fat=i["nutriments"]["fat_100g"],
-                                                         saturated_fat=i["nutriments"]["saturated-fat_100g"],
-                                                         sugars=i["nutriments"]["sugars_100g"],
-                                                         salt=i["nutriments"]["salt_100g"], )
+                    if i.get("product_name", False) and \
+                            i.get("brands", False) and \
+                            i.get("nutrition_grades", False) and \
+                            i.get("image_front_url", False):
+                        product = Product.objects.create(
+                            name=i["product_name"],
+                            brands=i["brands"],
+                            link=i["url"],
+                            nutriscore=i["nutrition_grades"],
+                            image=i["image_front_url"],
+                            fat=i["nutriments"]["fat_100g"],
+                            saturated_fat=i["nutriments"]
+                            ["saturated-fat_100g"],
+                            sugars=i["nutriments"]["sugars_100g"],
+                            salt=i["nutriments"]["salt_100g"], )
                         cat.products.add(product)
 
                 except IntegrityError:
