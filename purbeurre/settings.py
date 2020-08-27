86b94c0dd01a7e06085ac89a223f42b7c80daf15
Tony380 <tonygon2@yahoo.es>
2020-08-27 15:22:57 -0400

    Tests

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -145,11 +145,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
