f65b238688221050eb0bb4e05f2bc12d47f74f50
Tony380 <tonygon2@yahoo.es>
2020-08-28 14:44:43 -0400

    Tests

--- functional_tests/test_app.py
+++ functional_tests/test_app.py
@@ -1,55 +1,27 @@
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.common.keys import Keys
+import time
 
 
 class TestApp(StaticLiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
-
-    def tearDown(self):
-        self.browser.close()
-
-    def test_index_page(self):
-        self.browser.get(self.live_server_url)
-        alert = self.browser.find_element_by_class_name('text-center')
-        self.assertEquals(
-            alert.find_element_by_tag_name('h2').text,
-            'Trouvez un produit de substitution '
-            'pour ceux que vous consommez tous les jours'
-        )
+        self.driver = webdriver.Chrome()
 
     def test_register_form_submission_with_button(self):
+        self.driver.get(str(self.live_server_url) + '/user/register')
+        username_input = self.driver.find_element_by_id('id_username')
+        email_input = self.driver.find_element_by_id('id_email')
+        password1_input = self.driver.find_element_by_id('id_password1')
+        password2_input = self.driver.find_element_by_id('id_password2')
+        submission_button = self.driver.find_element_by_class_name('btn-primary')
 
-        self.browser.get(str(self.live_server_url) + '/user/register')
-        username_input = self.browser.find_element_by_id('id_username')
-        email_input = self.browser.find_element_by_id('id_email')
-        password1_input = self.browser.find_element_by_id('id_password1')
-        password2_input = self.browser.find_element_by_id('id_password2')
-        submission_button = self.browser.find_element_by_class_name(
-            'btn-primary')
-
-        username_input.send_keys('username')
-        email_input.send_keys('email@gmail.com')
-        password1_input.send_keys('abcdef123')
-        password2_input.send_keys('abcdef123')
+        username_input.send_keys('john')
+        email_input.send_keys('john@gmail.com')
+        password1_input.send_keys('123johnny123')
+        password2_input.send_keys('123johnny123')
         submission_button.click()
-        redirection_url = self.browser.current_url
-
-        self.assertEqual(self.live_server_url + '/user/register',
-                         redirection_url)
-
-    def test_login_form_submission_with_enter_key(self):
-        self.browser.get(str(self.live_server_url) + '/user/login')
-        username_input = self.browser.find_element_by_id('id_username')
-        password_input = self.browser.find_element_by_id('id_password')
-        submission_button = self.browser.find_element_by_class_name(
-            'btn-primary')
-
-        username_input.send_keys('username')
-        password_input.send_keys('abcdef123')
-        submission_button.send_keys(Keys.ENTER)
-        redirection_url = self.browser.current_url
-
-        self.assertEqual(self.live_server_url + '/user/login', redirection_url)
+        time.sleep(2)
+        redirection_url = self.driver.current_url
+        self.assertEquals(self.live_server_url + '/', redirection_url,
+                          'Bienvenu! Votre compte a été créé avec succès! Vous êtes maintenant connecté')
